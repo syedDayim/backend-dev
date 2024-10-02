2372e4b0563ee7122f739be110849b6a1642fcd2
syedDayim <dayim1277@gmail.com>
2024-10-02 15:45:06 +0530

    added connection.js setup app.js and env and cors and cookie and some middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dontenv from "dotenv";
 import connectDB from "./db/connection.js";
-
+import { app } from "./app.js";
 dontenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    // Will Listen Here
+    app.on("Error", (error) => {
+        console.log("Error", error);
+        throw error;
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening to http://localhost/${process.env.PORT}`);
+        
+    })
+})  
+.catch((error) => {
+    // Err will go here
+    console.log("MongoDB Connection Failed !!!", error);
+    
+})
